48b12427ccd7243c75f1938178c557f895f52e4e
Skogkatt <wayne79687968@gmail.com>
2021-01-28 23:40:10 +0800

    import vue

---
+++ Doctor/01.js
@@ -0,0 +1,6 @@
+export const first = {
+    "vitalSign"             : "T = 37.5°C, P = 105, R = 18, and BP= 105/70 mm Hg",
+    "pgysicalExamination"   : "恥骨聯合上有局部壓痛",
+    "cbc"                   : "Hgb 13.9 g/dL, Hct 41.7%, MCV 79 fL, WBC10,500μl",
+    "urineRoutine"          : "Bacteria 2+",
+};
