dcd83f80f4cc438c2823b27a80846bcfd61d39c8
Skogkatt <wayne79687968@gmail.com>
2021-01-30 11:08:43 +0800

    Fix: import js

--- Doctor/question/01.js
+++ Doctor/question/01.js
@@ -1,8 +1,8 @@
-var first = {
+var question_01 = {
     "vitalSign"             : "T = 37.5°C, P = 105, R = 18, and BP= 105/70 mm Hg",
     "pgysicalExamination"   : "恥骨聯合上有局部壓痛",
     "cbc"                   : "Hgb 13.9 g/dL, Hct 41.7%, MCV 79 fL, WBC10,500μl",
     "urineRoutine"          : "Bacteria 2+",
 };
 
-export default first;
+export default question_01;
--- Doctor/question/index.js
+++ Doctor/question/index.js
@@ -1,1 +1,6 @@
-import '/01.js';
+import question_01 from './01.js';
+
+var question = [];
+question = question.concat(question_01);
+
+export default question;
